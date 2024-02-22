5e6c162ce31de6a502751b293b2ea461fb37c1ec
unknown <ansarizawed4@gmail.com>
2024-02-22 11:47:36 +0530

    modified code for mail

--- Library-Management-System/src/main/java/com/example/Library/Management/System/Services/StudentService.java
+++ Library-Management-System/src/main/java/com/example/Library/Management/System/Services/StudentService.java
@@ -4,8 +4,8 @@ import com.example.Library.Management.System.Entities.Student;
 import com.example.Library.Management.System.Repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-//import org.springframework.mail.SimpleMailMessage;
-//import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @Service
 public class StudentService {
@@ -13,23 +13,23 @@ public class StudentService {
     private StudentRepository studentRepository;
 
     @Autowired
- //   private JavaMailSender mailSender;
+    private JavaMailSender mailSender;
 
     public String addStudent(Student student){
-//
-//        studentRepository.save(student);
-//
-//        SimpleMailMessage mailMessage = new SimpleMailMessage();
-//
-//        String body = "Hi "+student.getName()+" !" +
-//                "You have successfully registered. You can start issuing the books now.";
-//
-//        mailMessage.setFrom("springacciojob@gmail.com");
-//        mailMessage.setTo(student.getEmailId());
-//        mailMessage.setSubject("Welcome To St Xavier's School's Library !!");
-//        mailMessage.setText(body);
-//
-//        mailSender.send(mailMessage);
+
+        studentRepository.save(student);
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        String body = "Hi "+student.getName()+" !" +
+                "You have successfully registered. You can start issuing the books now.";
+
+        mailMessage.setFrom("springacciojob@gmail.com");
+        mailMessage.setTo(student.getEmailId());
+        mailMessage.setSubject("Welcome To St Xavier's School's Library !!");
+        mailMessage.setText(body);
+
+       mailSender.send(mailMessage);
         return "Student has been saved to the DB";
     }
 }
