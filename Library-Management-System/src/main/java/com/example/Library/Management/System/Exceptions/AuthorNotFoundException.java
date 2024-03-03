24e0d6ab81af99dc4f22bcca9aebe6f066d8ea64
unknown <ansarizawed4@gmail.com>
2024-03-03 18:15:57 +0530

    swagger added

--- Library-Management-System/src/main/java/com/example/Library/Management/System/Exceptions/AuthorNotFoundException.java
+++ Library-Management-System/src/main/java/com/example/Library/Management/System/Exceptions/AuthorNotFoundException.java
@@ -2,6 +2,7 @@ package com.example.Library.Management.System.Exceptions;
 
 public class AuthorNotFoundException extends Exception{
     public AuthorNotFoundException(String message) {
+
         super(message);
     }
 }
